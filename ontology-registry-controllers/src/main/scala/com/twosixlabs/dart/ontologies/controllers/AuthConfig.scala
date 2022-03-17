7e4da977e2c2c56b95ae75554b9f259e3e7ed8d9
johnhungerford <hungerfordjustice@gmail.com>
2022-03-17 16:48:44 -0400

    Fixed auth config failure

--- ontology-registry-controllers/src/main/scala/com/twosixlabs/dart/ontologies/controllers/AuthConfig.scala
+++ ontology-registry-controllers/src/main/scala/com/twosixlabs/dart/ontologies/controllers/AuthConfig.scala
@@ -2,4 +2,7 @@ package com.twosixlabs.dart.ontologies.controllers
 
 import com.twosixlabs.dart.auth.controllers.SecureDartController.AuthDependencies
 
-case class AuthConfig( override val secretKey : Option[ String ], override val bypassAuth : Boolean ) extends AuthDependencies
+case class AuthConfig( override val secretKey : Option[ String ],
+					   override val useDartAuth : Boolean,
+					   override val basicAuthCredentials : Seq[ (String, String) ] = Nil )
+  extends AuthDependencies
